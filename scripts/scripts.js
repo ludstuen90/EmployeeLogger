eee0090497da171b78c814ed041704a22cb07649
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-05-29 16:05:38 -0500

    Table Add Now Works

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,9 +16,27 @@ var newEmpl = {
 };
 
 employees.push(newEmpl);
-
-
+displayEmployees();
 
   console.log(employees);
 
 };
+
+
+
+
+
+var displayEmployees = function() {
+document.getElementById("draw").innerHTML = '';
+
+for (var i = 0; i < employees.length; i++) {
+  var zfname = "First Name: " + employees[i]["First Name"];
+  var zlname = "Last Name: " + employees[i]["Last Name"];
+  var zempl = "EMPL ID: " + employees[i]["Employee ID"];
+  var zsal= "Salary: " + employees[i]["Salary"];
+  document.getElementById("draw").innerHTML += (zfname + zlname + zempl + zsal);
+}
+
+};
+
+displayEmployees();
