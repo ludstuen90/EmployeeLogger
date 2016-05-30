9ad7d74daae637deceb1bfeb14cde0df50b2a326
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-05-30 17:44:30 -0500

    Added delete button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,14 @@
-console.log("Hola, mundo!");
-
 var employees = [];
+
+var deleteEmpl = function(fired) {
+  if (confirm("Are you sure you want to remove Employee " + employees[fired]["First Name"] + " "+  employees[fired]["Last Name"] + "?")) {
+    employees.splice(fired, 1);
+  }
+  displayEmployees();
+
+};
+
+
 
 var addEmpl= function() {
   var firstName = document.getElementById("fName").value;
@@ -18,8 +26,6 @@ var newEmpl = {
 employees.push(newEmpl);
 displayEmployees();
 
-
-
 document.getElementById("fName").value=null;
 document.getElementById("lName").value=null;
 document.getElementById("emplId").value=null;
@@ -27,14 +33,8 @@ document.getElementById("emplId").value=null;
 
 };
 
-
-
-
-
 var displayEmployees = function() {
-
-  var htmloutput = '<table style="width:100%"><tr><td><b>First Name:</td><td><b>Last Name:</b></td><td><b>Employee ID:</b></td><td><b>Salary:</b></td></tr>';
-
+  var htmloutput = '<table style="width:100%"><tr><td><b>First Name:</td><td><b>Last Name:</b></td><td><b>Employee ID:</b></td><td><b>Salary:</b></td><td><b>Delete Employee:</b></td></tr>';
 var totalSalary = 0;
 
 for (var i = 0; i < employees.length; i++) {
@@ -42,26 +42,20 @@ for (var i = 0; i < employees.length; i++) {
   var zlname = employees[i]["Last Name"];
   var zempl = employees[i]["Employee ID"];
   var zsal= employees[i]["Salary"];
+  var rmButton = '<button onclick="deleteEmpl(' + i + ')">Delete Employee</button>';
 
-  htmloutput +=  '<tr><td>' + zfname +  '</td> <td>'+ zlname +'</td> <td>' + zempl +'</td> <td>'+ zsal + '</td> </tr>';
-
-  if (zsal == undefined){}
+  htmloutput +=  '<tr><td>' + zfname +  '</td> <td>'+ zlname +'</td> <td>' + zempl +'</td> <td>'+ zsal + '</td><td>' + rmButton+ '</td> </tr>';
+  if (zsal === undefined){}
   else {
     totalSalary += parseFloat(zsal);
-
   }
-
-
 }
 
 
-htmloutput += '</table>'
+
+htmloutput += '</table>';
 document.getElementById("draw").innerHTML = htmloutput;
-
-
-
-document.getElementById("bank").innerHTML = totalSalary;
-
+document.getElementById("bank").innerHTML = '<table style="width:100%"><tr><td><b><p>Total Salary Expense: </b>' + totalSalary + '</p></td></tr></table>';
 
 };
 
